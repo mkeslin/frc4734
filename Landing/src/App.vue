a0907c3b8730ac2cc770184e1cb46076f07c3e7f
mkeslin <mkeslin@outlook.com>
2023-09-19 18:44:00 -0500

    Landing: added links

--- Landing/src/App.vue
+++ Landing/src/App.vue
@@ -1,38 +1,48 @@
 <template>
   <div>
     <h1>Iron Plainsmen Robotics</h1>
-    <img style="margin-top: 24px;" src="./assets/Construction2.gif" alt="construction 1" />
-    <!-- <br /> -->
-    <div style="display:flex; align-items: center; justify-content: space-around;">
+    <img
+      style="margin-top: 24px"
+      src="./assets/Construction2.gif"
+      alt="construction 1"
+    />
+    <div
+      style="display: flex; align-items: center; justify-content: space-around"
+    >
       <img src="./assets/Construction4.gif" alt="construction 1" />
       <img src="./assets/Construction3.gif" alt="construction 1" />
       <img src="./assets/Construction1.gif" alt="construction 1" />
       <img src="./assets/Construction3.gif" alt="construction 1" />
       <img src="./assets/Construction4.gif" alt="construction 1" />
     </div>
-    <!-- <br /> -->
     <img src="./assets/Construction2.gif" alt="construction 1" />
   </div>
   <div>
     <h2 style="margin-top: 60px; margin-bottom: 12px">Links</h2>
-    <!-- <ul> -->
-    <!-- <li> -->
     <a
       href="https://plainsmenroboticsl9.wixsite.com/iprobotics4734"
       target="_blank"
+      >Legacy Site</a
     >
-      Legacy Site
-    </a>
     <br />
-    <!-- </li> -->
-    <!-- <li> -->
-    <a href="https://scouting.frc4734.com" target="_blank"> Scouting Site </a>
-    <!-- </li> -->
-    <!-- </ul> -->
+    <a href="https://scouting.frc4734.com" target="_blank"
+      >Scouting Site (in development)</a
+    >
+    <br />
+    <a
+      href="https://ironplainsmen-el92228.slack.com/ssb/redirect?_gl=1*zt37xp*_gcl_au*MjExMjk2MzA5My4xNjkzOTExMjAw"
+      target="_blank"
+      >Slack</a
+    >
+    <br />
+    <a href="https://github.com/mkeslin/frc4734" target="_blank"
+      >2023-2024 Source Code</a
+    >
+    <br />
+    <a href="https://github.com/NickAtNight11/Charged2023" target="_blank"
+      >2022-2023 Source Code</a
+    >
   </div>
-  <!--<a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
