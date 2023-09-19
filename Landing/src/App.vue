3f32b57a34750d3cc8aec5c6f26cff4111a1cdaa
mkeslin <mkeslin@outlook.com>
2023-09-19 17:48:41 -0500

    Added landing page

---
+++ Landing/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Iron Plainsmen Robotics</h1>
+    <img src="./assets/Construction2.gif" alt="construction 1" />
+    <br />
+    <img src="./assets/Construction4.gif" alt="construction 1" />
+    <img src="./assets/Construction3.gif" alt="construction 1" />
+    <img src="./assets/Construction1.gif" alt="construction 1" />
+    <img src="./assets/Construction3.gif" alt="construction 1" />
+    <img src="./assets/Construction4.gif" alt="construction 1" />
+    <br />
+    <img src="./assets/Construction2.gif" alt="construction 1" />
+  </div>
+  <div>
+    <h2 style="margin-top: 24px">Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://plainsmenroboticsl9.wixsite.com/iprobotics4734"
+          target="_blank"
+        >
+          Legacy Site
+        </a>
+      </li>
+      <!-- <li>
+        <a href="https://scouting.frc4734.com" target="_blank">
+          Scouting Site
+        </a>
+      </li> -->
+    </ul>
+  </div>
+  <!--<a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<script setup lang="ts">
+// import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
