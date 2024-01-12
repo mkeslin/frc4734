6e7ad1615e450b2ca1840681df4257ce11198a41
Matthew Keslin <mkeslin@outlook.com>
2024-01-12 06:38:09 -0600

    Integrated PathPlanner

--- 2024-Crescendo/src/main/java/frc/robot/RobotContainer.java
+++ 2024-Crescendo/src/main/java/frc/robot/RobotContainer.java
@@ -3,16 +3,19 @@ package frc.robot;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
-
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-
 import frc.robot.Constants.ControllerIds;
-import frc.robot.SwerveDrivetrain.CommandSwerveDrivetrain;
 import frc.robot.SwerveDrivetrain.*;
 
 public class RobotContainer {
@@ -44,12 +47,27 @@ public class RobotContainer {
     SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
     Telemetry logger = new Telemetry(MaxSpeed);
 
+    // PathPlanner
+    private final SendableChooser<Command> autoChooser;
+
     public RobotContainer() {
         // initialize subsystems
         // horizontalElevator =
         //     new Elevator("horizontal", HORELEVATOR1ID, HORELEVATOR2ID, -200, -14000, -24000);
         // verticalElevator =
         //     new Elevator("vertical", VERTELEVATOR1ID, VERTELEVATOR2ID, 3000, 45000, 45000);
+
+        // Register Named Commands
+        // NamedCommands.registerCommand("autoBalance", swerve.autoBalanceCommand());
+        // NamedCommands.registerCommand("exampleCommand", exampleSubsystem.exampleCommand());
+        // NamedCommands.registerCommand("someOtherCommand", new SomeOtherCommand());
+
+        // PathPlanner
+        // Build an auto chooser. This will use Commands.none() as the default option.
+        autoChooser = AutoBuilder.buildAutoChooser();
+        // Another option that allows you to specify the default auto by its name
+        // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+        SmartDashboard.putData("Auto Chooser", autoChooser);
 
         // configure bindings
         configureBindings_DriveTrain();
@@ -162,5 +180,13 @@ public class RobotContainer {
 
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
+        // return new PathPlannerAuto("Example Auto");
+        // return autoChooser.getSelected();
+
+        // // Load the path you want to follow using its name in the GUI
+        // var path = PathPlannerPath.fromPathFile("Example Path");
+
+        // // Create a path following command using AutoBuilder. This will also trigger event markers.
+        // return AutoBuilder.followPath(path);
     }
 }
--- 2024-Crescendo/src/main/java/frc/robot/SwerveDrivetrain/CommandSwerveDrivetrain.java
+++ 2024-Crescendo/src/main/java/frc/robot/SwerveDrivetrain/CommandSwerveDrivetrain.java
@@ -1,38 +1,88 @@
 package frc.robot.SwerveDrivetrain;
 
-import java.util.function.Supplier;
-
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
-
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import java.util.function.Supplier;
 
 /**
  * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem
  * so it can be used in command-based projects easily.
  */
 public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsystem {
+
     private static final double kSimLoopPeriod = 0.005; // 5 ms
     private Notifier m_simNotifier = null;
     private double m_lastSimTime;
 
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
+    public CommandSwerveDrivetrain(
+        SwerveDrivetrainConstants driveTrainConstants,
+        double OdometryUpdateFrequency,
+        SwerveModuleConstants... modules
+    ) {
         super(driveTrainConstants, OdometryUpdateFrequency, modules);
         if (Utils.isSimulation()) {
             startSimThread();
         }
+        configurePathPlanner();
     }
-    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
+
+    public CommandSwerveDrivetrain(
+        SwerveDrivetrainConstants driveTrainConstants,
+        SwerveModuleConstants... modules
+    ) {
         super(driveTrainConstants, modules);
         if (Utils.isSimulation()) {
             startSimThread();
         }
+        configurePathPlanner();
+    }
+
+    private void configurePathPlanner() {
+        // PathPlanner
+        AutoBuilder.configureHolonomic(
+            // Robot pose supplier
+            this::getPose,
+            // Method to reset odometry (will be called if your auto has a starting pose)
+            this::resetPose,
+            // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            this::getRobotRelativeSpeeds,
+            // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+            this::driveRobotRelative,
+            // HolonomicPathFollowerConfig, this should likely live in your Constants class
+            new HolonomicPathFollowerConfig(
+                new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                4.5, // Max module speed, in m/s
+                0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                new ReplanningConfig(), // Default path replanning config. See the API for the options here,
+                0.2
+            ),
+            () -> {
+                // Boolean supplier that controls when the path will be mirrored for the red alliance
+                // This will flip the path being followed to the red side of the field.
+                // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+                var alliance = DriverStation.getAlliance();
+                if (alliance.isPresent()) {
+                    return alliance.get() == DriverStation.Alliance.Red;
+                }
+                return false;
+            },
+            this // Reference to this subsystem to set requirements
+        );
     }
 
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
@@ -43,14 +93,34 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
         m_lastSimTime = Utils.getCurrentTimeSeconds();
 
         /* Run simulation at a faster rate so PID gains behave more reasonably */
-        m_simNotifier = new Notifier(() -> {
-            final double currentTime = Utils.getCurrentTimeSeconds();
-            double deltaTime = currentTime - m_lastSimTime;
-            m_lastSimTime = currentTime;
+        m_simNotifier =
+            new Notifier(() -> {
+                final double currentTime = Utils.getCurrentTimeSeconds();
+                double deltaTime = currentTime - m_lastSimTime;
+                m_lastSimTime = currentTime;
 
-            /* use the measured time delta, get battery voltage from WPILib */
-            updateSimState(deltaTime, RobotController.getBatteryVoltage());
-        });
+                /* use the measured time delta, get battery voltage from WPILib */
+                updateSimState(deltaTime, RobotController.getBatteryVoltage());
+            });
         m_simNotifier.startPeriodic(kSimLoopPeriod);
     }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // For use with PathPlanner
+    private Pose2d getPose() {
+        return this.getPose();
+    }
+
+    private void resetPose(Pose2d pose) {
+        this.resetPose(pose);
+    }
+
+    private ChassisSpeeds getRobotRelativeSpeeds() {
+        return this.getRobotRelativeSpeeds();
+    }
+
+    private void driveRobotRelative(ChassisSpeeds chassisSpeeds) {
+        this.driveRobotRelative(chassisSpeeds);
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
