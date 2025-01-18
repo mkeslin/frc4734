7dc888bed5e163b695cf65a392ba0540d2a8b943
100148411 <100148411@lubbockisd.net>
2025-01-18 15:53:20 -0600

    Updated practice swerve constants

--- 2025-Reefscape/PracticeSwerve/src/main/java/frc/robot/SwerveDrivetrain/SwerveDrivetrainA.java
+++ 2025-Reefscape/PracticeSwerve/src/main/java/frc/robot/SwerveDrivetrain/SwerveDrivetrainA.java
@@ -100,7 +100,7 @@ public class SwerveDrivetrainA {
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
-    private static final double kCoupleRatio = 2;
+    private static final double kCoupleRatio = 3.5;
 
     private static final double kDriveGearRatio = 48;
     private static final double kSteerGearRatio = 24;
