6c8dd85b82b179889b247623f6a5425566259504
100148411 <100148411@lubbockisd.net>
2025-01-25 16:46:56 -0600

    test commit 2

--- 2025-Reefscape/PracticeSwerve/src/main/java/frc/robot/SwerveDrivetrain/DrivetrainConstants.java
+++ 2025-Reefscape/PracticeSwerve/src/main/java/frc/robot/SwerveDrivetrain/DrivetrainConstants.java
@@ -4,6 +4,6 @@ public class DrivetrainConstants {
 
     public static final double MaxSpeed = 0.2; //SwerveDrivetrainA.kSpeedAt12VoltsMps; // meters per second desired top speed
     public static final double MaxAngularRate = .1 * Math.PI; // 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
-    // test
+    
     public static final double MaxAcceleration = .4; //2;
 }
